f060658e8962eb7b240bc47a00cfa3004e75fcf8
Grish <36304554+justmavi@users.noreply.github.com>
2022-05-12 17:00:25 +0400

    feat: GET methods implemenatation for ratings (#48)
    * feat: rating initial implementation
    
    * fix: bug related inserted ratings returning
    
    * fix: upsert bug
    
    * feat: GET methods implemenatation for ratings
    
    Co-authored-by: Grisha Poghosyan <grisha.poghosyan@softconstruct.com>

--- src/rating/rating.controller.ts
+++ src/rating/rating.controller.ts
@@ -2,26 +2,40 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   NotFoundException,
+  Param,
   ParseArrayPipe,
   Post,
   Query,
 } from '@nestjs/common';
 import { BatchDelete } from 'src/common/types/batch-delete.type';
 import { IOk } from 'src/common/types/ok.type';
+import { PathParams } from 'src/common/types/path-params.type';
 import { RatingService } from 'src/rating/rating.service';
 import { UpsertRatingDto } from './dto/upsert-rating.dto';
+import { RatingFilter } from './types/rating-filter.type';
 
 @Controller('rating')
 export class RatingController {
   constructor(private readonly ratingService: RatingService) {}
 
   @Post()
-  async create(
+  async upsert(
     @Body(new ParseArrayPipe({ items: UpsertRatingDto }))
     upsertRatingDtos: Array<UpsertRatingDto>,
   ) {
     return await this.ratingService.upsert(upsertRatingDtos);
+  }
+
+  @Get()
+  async findAll(@Query() filters: RatingFilter) {
+    return await this.ratingService.findAll(filters);
+  }
+
+  @Get(':id')
+  async findOne(@Param() { id }: PathParams) {
+    return await this.ratingService.findOne(id);
   }
 
   @Delete()
