26719667a0cefc7f422059d6f75f26f4b4f106e6
Grish <grisha.poghosyan.dev@gmail.com>
2022-08-09 10:11:40 -0700

    fix: cli script on win32

--- cli/add-migration.js
+++ cli/add-migration.js
@@ -1,24 +1,31 @@
 /**
  * The script automates TypeORM migrations generating process
  * You don't need to input data source path and other parameters yourself
- * Just use `npm run addming --name <migration name> --module <module name>` and enjoy
+ * Just use `npm run addming -- --name <migration name> --module <module name>` and enjoy
  * © justmavi 2022
  */
 
 const path = require('path');
 const fs = require('fs');
+const mv = require('mv');
 const { spawn } = require('child_process');
 const yargs = require('yargs');
+
+const ROOT_FOLDER_NAME = 'src';
+const DIST_FOLDER_NAME = 'dist';
+const MIGRATIONS_FOLDER_NAME = 'migrations';
+const DATA_SOURCE_FILE_NAME = 'data-source.js';
+const NPX_COMMAND = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
 
 const { name, module: moduleName } = yargs(process.argv).argv;
 
 if (!name) {
   exitWithMessage(
-    'You must enter migration name. npm run addmig --name <name>',
+    'You must enter migration name. npm run addmig -- --name <name>',
   );
 } else if (!moduleName) {
   exitWithMessage(
-    'You must enter module name. npm run addmig --module <module>',
+    'You must enter module name. npm run addmig -- --module <module>',
   );
 }
 
@@ -27,12 +34,12 @@ if (!fs.existsSync(path.join(process.cwd(), 'src', moduleName))) {
 }
 
 const timestamp = Date.now();
-const ls = spawn('npx', [
+const ls = spawn(NPX_COMMAND, [
   'typeorm',
   'migration:generate',
-  `src/${moduleName}/migrations/${name}`,
+  path.join(ROOT_FOLDER_NAME, moduleName, MIGRATIONS_FOLDER_NAME, name),
   '-d',
-  'dist/data-source.js',
+  path.join(DIST_FOLDER_NAME, DATA_SOURCE_FILE_NAME),
   '-t',
   timestamp,
 ]);
@@ -47,9 +54,9 @@ ls.stdout.on('data', (data) => {
   const filename = `${timestamp}-${name}.ts`;
   const migrationPath = path.join(
     process.cwd(),
-    'src',
+    ROOT_FOLDER_NAME,
     moduleName,
-    'migrations',
+    MIGRATIONS_FOLDER_NAME,
     filename,
   );
 
@@ -59,10 +66,9 @@ ls.stdout.on('data', (data) => {
 
   const newFilename = `${timestamp}-${slugifiedName}.migration.ts`;
   const newMigrationPath = migrationPath.replace(filename, newFilename);
-  const ls = spawn('mv', [migrationPath, newMigrationPath]);
 
-  ls.on('close', (code) => {
-    if (code !== 0) {
+  mv(migrationPath, newMigrationPath, (err) => {
+    if (err) {
       exitWithMessage(
         'Error occured while renaming migration file. Maybe, permission denied',
       );
@@ -71,7 +77,7 @@ ls.stdout.on('data', (data) => {
       `Migration ${newMigrationPath} has been generated successfully.`,
     );
 
-    const ls = spawn('npx', ['prettier', '--write', newMigrationPath]);
+    const ls = spawn(NPX_COMMAND, ['prettier', '--write', newMigrationPath]);
 
     ls.on('close', (code) => {
       if (code !== 0) {
